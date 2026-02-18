0c1f505739173f579640c0936202639a650e0660
keisuke.okafuji <keiokafuj@digital.go.jp>
2026-02-18 13:27:13 +0900

    feat(Carousel): add CarouselSingle component, Container/KeyVisual types and link/no-link story variations

--- src/components/Carousel/index.ts
+++ src/components/Carousel/index.ts
@@ -1,2 +1,8 @@
 export type { CarouselProps, CarouselSlide } from './Carousel';
 export { Carousel } from './Carousel';
+export type {
+  CarouselSingleImageProps,
+  CarouselSingleLinkProps,
+  CarouselSingleProps,
+} from './CarouselSingle';
+export { CarouselSingle, CarouselSingleImage, CarouselSingleLink } from './CarouselSingle';
