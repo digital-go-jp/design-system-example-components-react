355fda1d6006a5c9f7160494ccb5137d9d023c71
SHIMADA Takayuki <takayshim@digital.go.jp>
2026-06-15 15:06:44 +0900

    feat: add Foundations docs pages for color, typography, and elevation

---
+++ src/foundations/Elevation.tsx
@@ -0,0 +1,42 @@
+export const Elevation = () => {
+  return (
+    <div className='prose'>
+      <h1>エレベーション</h1>
+      <p>デジタル庁デザインシステムで定義されているエレベーションスタイルの一覧です。</p>
+      <div className='mt-8 flex flex-wrap gap-12'>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-1'}></div>
+          <span className='text-std-16B-170 self-center'>Style 1</span>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-2'}></div>
+          <span className='text-std-16B-170 self-center'>Style 2</span>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-3'}></div>
+          <span className='text-std-16B-170 self-center'>Style 3</span>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-4'}></div>
+          <span className='text-std-16B-170 self-center'>Style 4</span>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-5'}></div>
+          <span className='text-std-16B-170 self-center'>Style 5</span>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-6'}></div>
+          <span className='text-std-16B-170 self-center'>Style 6</span>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-7'}></div>
+          <span className='text-std-16B-170 self-center'>Style 7</span>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className={'size-48 rounded-12 shadow-8'}></div>
+          <span className='text-std-16B-170 self-center'>Style 8</span>
+        </div>
+      </div>
+    </div>
+  );
+};
