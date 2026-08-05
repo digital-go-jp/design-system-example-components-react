5adac6d9c13d97525b7b8c4ffb1d2675653ba296
keisuke.okafuji <1397228+johnykei@users.noreply.github.com>
2026-08-05 16:45:28 +0900

    fix(StatusBadge): roundedはDADSトークンを使用する

--- src/components/StatusBadge/StatusBadge.tsx
+++ src/components/StatusBadge/StatusBadge.tsx
@@ -5,7 +5,7 @@ export const StatusBadge = (props: ComponentProps<'span'>) => {
 
   return (
     <span
-      className={`ml-2 inline-block rounded-lg bg-solid-gray-536 p-2 text-oln-16N-100 text-white outline-1 outline-transparent ${className ?? ''}`}
+      className={`ml-2 inline-block rounded-8 bg-solid-gray-536 p-2 text-oln-16N-100 text-white outline-1 outline-transparent ${className ?? ''}`}
       {...rest}
     >
       {children}
